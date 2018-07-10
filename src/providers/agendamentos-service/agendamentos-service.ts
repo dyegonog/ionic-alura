c8829208b94bb0ef60913ee738047b8b399ac46e
Dyego Nogueira <dyego@sedep.com.br>
2018-07-10 16:57:01 -0300

    observable

--- src/providers/agendamentos-service/agendamentos-service.ts
+++ src/providers/agendamentos-service/agendamentos-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Agendamento } from '../../modelos/agendamento';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AgendamentosServiceProvider {
